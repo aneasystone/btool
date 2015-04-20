d1293ee3f4484d1a5ed672b33181bd7906eb2029
aneasystone <aneasystone@gmail.com>
2015-04-20 17:20:29 +0800

    first push

---
+++ btool.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 19:15:34 2015
+
+@author: aneasystone
+"""
+
+import transformor
+
+def main():
+    
+    link = transformor.torrent2magnet('torrents\\ubuntu.torrent')
+    print link
+    
+    r = transformor.magnet2torrent(
+        'magnet:?xt=urn:btih:1619ecc9373c3639f4ee3e261638f29b33a6cbd6&dn=ubuntu-14.10-desktop-i386.iso', 
+        't.torrent')
+    if r == True:
+        print 'Torrent created successfully!'
+    else:
+        print 'Torrent created Failed!'
+
+ 
+if __name__ == '__main__':
+    main()
